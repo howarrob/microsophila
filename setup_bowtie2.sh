3979dce589e57694080e45fb40f9c7917a212ff7
Ammon Knaupp <62727673+adknaupp@users.noreply.github.com>
2023-03-29 21:42:01 -0600

    Add lines which set up reference sequence

--- setup_bowtie2.sh
+++ setup_bowtie2.sh
@@ -13,3 +13,15 @@ mv $name_to_replace bowtie2
 
 # remove .zip file
 rm download
+
+# set up reference sequence
+mkdir "$MICROSOPHLIA_STORAGE_PATH/ref"
+dm_ref_path="$MICROSOPHLIA_STORAGE_PATH/ref/d_melanogaster"
+mkdir "$dm_ref_path"
+
+# download
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/215/GCF_000001215.4_Release_6_plus_ISO1_MT/GCF_000001215.4_Release_6_plus_ISO1_MT_genomic.fna.gz > "$dm_ref_path/GCF_000001215.4_Release_6_plus_ISO1_MT_genomic.fna.gz"
+gunzip "$dm_ref_path/GCF_000001215.4_Release_6_plus_ISO1_MT_genomic.fna.gz"
+
+# build index
+bowtie2-build -f "$dm_ref_path/GCF_000001215.4_Release_6_plus_ISO1_MT_genomic.fna" "$dm_ref_path/bt2_index"
